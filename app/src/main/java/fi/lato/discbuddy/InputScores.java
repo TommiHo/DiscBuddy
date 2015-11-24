751b917c9a8d2daabb006faff116ffbfb3410356
Lasse Jokinen <lasse.jokine@gmail.com>
2015-11-24 15:45:41 +0200

    fixed merge

--- app/src/main/java/fi/lato/discbuddy/InputScores.java
+++ app/src/main/java/fi/lato/discbuddy/InputScores.java
@@ -2,6 +2,8 @@ package fi.lato.discbuddy;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.support.v13.app.FragmentPagerAdapter;
@@ -13,8 +15,10 @@ import android.widget.ListView;
  * Created by tommi on 10.11.2015.
  */
 public class InputScores extends FragmentActivity {
-
+    private SQLiteDatabase db;
     private ListView scores_listView;
+    private final String DATABASE_PLAYERS = "players";
+    private final String DATABASE_SCORES = "scores";
     //public static ArrayList<Player> playerNames;
 
 
@@ -50,41 +54,6 @@ public class InputScores extends FragmentActivity {
 
 
         // tuloksien syöttäminen
-
-    public void plusbuttonClick(View v){
-        int position = scores_listView.getPositionForView(v);
-        TextView tv = (TextView) scores_listView.getChildAt(position).findViewById(R.id.par);
-        tv.setText(String.valueOf(Integer.parseInt(tv.getText().toString())+1));
-    }
-
-    public void minusbuttonClick(View v){
-        int position = scores_listView.getPositionForView(v);
-        TextView tv = (TextView) scores_listView.getChildAt(position).findViewById(R.id.par);
-        tv.setText(String.valueOf(Integer.parseInt(tv.getText().toString())-1));
-    }
-
-    // query data from database
-    public void queryData(ArrayList<String> playerNames) {
-        String[] names = new String[playerNames.size()];
-        names = playerNames.toArray(names);
-        //names =playerNames.to
-        cursor = db.rawQuery("SELECT _id, name FROM players WHERE name IN (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", names);
-        // get data with query
-        String[] resultColumns = new String[]{"_id","name"};
-        //String[] scoreColumns = new String[]{"_id","hole","holeScore","courseScore","_id"};
-        cursor = db.query(DATABASE_PLAYERS,resultColumns,null,null,null,null,null);
-        //cursor = db.query(DATABASE_SCORES,scoreColumns,null,null,null,null,null);
-        // add data to adapter
-        ListAdapter adapter = new SimpleCursorAdapter(this,
-                R.layout.score_list_item, cursor,
-                new String[] {"name"},      // from
-                new int[] {R.id.name}    // to
-                ,0);  // flags
-
-        // show data in listView
-        //scores_listView.setAdapter(adapter);
-        Log.v("Cursor", DatabaseUtils.dumpCursorToString(cursor));
-    }
 
     public void onPageScrollStateChanged(int state, String name, Integer par) {
         ContentValues values=new ContentValues(3);
